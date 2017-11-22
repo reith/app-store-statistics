6260957bd074068bcfb5e731cc080184a96f5e90
Amir Ghassemi Nasr <Ameretat.Reith@gmail.com>
2017-11-22 10:34:08 +0330

    drop JPA implementation

--- src/appstatistics/AppStatisticsCrud.kt
+++
@@ -1,9 +0,0 @@
-package appstatistics
-
-import org.springframework.data.repository.CrudRepository
-import java.util.*
-
-interface AppStatisticsCrud : CrudRepository<AppStatistics, String> {
-    fun findByType(type: Int) : List<AppStatistics>
-    fun findByTypeAndReportTimeBetween(type: Int, startDate: Date, endDate: Date): List<AppStatistics>
-}
